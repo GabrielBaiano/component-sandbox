2c4b03ca616685cfa6ce029c8d27491a47d54b3c
GabrielBaiano <gabrielngama@gmail.com>
2026-04-22 08:48:38 -0300

    feat(calendar): add scketch of a calendar component!

---
+++ src/components/Calendar/index.tsx
@@ -0,0 +1,7 @@
+export default function Calendar() {
+  return (
+    <div className="flex flex-col items-center">
+      <h2 className="font-mono text-[10px] uppercase tracking-tighter">test</h2>
+    </div>
+  );
+}
---
+++ src/components/Calendar/meta.ts
@@ -0,0 +1,11 @@
+import type { ComponentMeta } from '../../types';
+
+export const meta: ComponentMeta = {
+  id: 'calendar',
+  name: 'Calendar Class',
+  description: 'A simple calendar',
+  tags: ['calendar', 'minimalistic'],
+  tech: ['react', 'tailwind'],
+  createdAt: '2026-22-04',
+  props: [],
+};
